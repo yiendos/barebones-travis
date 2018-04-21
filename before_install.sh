17363772c80e67cf79d2e41d0e730d7ca8352091
yiendos <yiendos@sbmatrix.com>
2018-04-21 13:20:56 +0100

    extend before to include joomla console site:create

--- before_install.sh
+++ before_install.sh
@@ -20,8 +20,19 @@ chmod +x /home/travis/chromedriver
 /home/travis/chromedriver --verbose --log-path=/tmp/chromedriver.log --url-base=/wd/hub &
 
 echo "Installing the test dependencies"
+mkdir -p $DOCUMENTROOT $PROJECT_DIR
+composer global require --no-interaction codeception/codeception:2.3.4 facebook/webdriver:1.4.1 flow/jsonpath joomlatools/console
+joomla plugin:install joomlatools/console-joomlatools:dev-master
 
-composer global require --no-interaction codeception/codeception:2.3.4 facebook/webdriver:1.4.1
+DEPENDENCIES=('joomlatools-framework' 'joomlatools-framework-files' 'joomlatools-framework-activities' 'joomlatools-framework-scheduler' 'joomlatools-framework-ckeditor' 'joomlatools-framework-tags' 'joomlatools-framework-migrator' 'joomlatools-ui' 'logman' 'fileman' 'connect')
+for DEPENDENCY in "${DEPENDENCIES[@]}"
+do
+  git clone -q -b master git@github.com:joomlatools/$DEPENDENCY.git $PROJECT_DIR/$DEPENDENCY
+done
+
+echo "Installing Joomla $JOOMLA"
+joomla site:create --projects-dir=$PROJECT_DIR --release=$JOOMLA --symlink=textman,logman,fileman,connect --www=$DOCUMENTROOT --mysql-login="root" home
+
 
 ## starting in built server
 echo "Starting the PHP webserver"
